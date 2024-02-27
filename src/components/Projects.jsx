4d37b9acdc6f24c5e2e9193808d78a71dc39ab5b
lautaroblasco23 <lautaroblasco98@gmail.com>
2024-02-27 18:20:39 -0300

    progress

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,16 +9,15 @@ const Projects = () => {
     .then(data => data.json())
     .then((data) => {
         data = data.filter((repo) => {
-          return !repo.description.includes("code")
+          return !repo.description.includes("personal project")
         })
-        console.log(data);
         setRepos(data);
     })
   }, []);
   
   return (
     <div id="projects-container" className="projects-container h-screen w-screen flex flex-col items-center ">
-      <h2 className="text-4xl mt-8 mx-12">Apps I've developed</h2>
+      <h2 className="text-4xl mt-8 mx-12">Apps I've developed (not ready)</h2>
       <div className="w-full h-full mt-4 grid grid-cols-3 grid-rows-2 place-items-center mx-auto">
         {
           repos.map((repo) => {
