265bc3159be277736d249c12f55541f37c6b6709
lautaroblasco23 <lautaroblasco98@gmail.com>
2024-02-27 18:20:39 -0300

    progress

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 
 const Contact = () => {
+
   return (
-    <div className="w-screen h-screen flex items-center justify-center ">
-      <div className="rounded-br-2xl bg-blue-200 border border-black p-4 flex-col flex justify-center md:h-[400px] md:w-[800px]">
-        <h2 className="mx-auto text-2xl font-bold">Get In Touch!</h2>
-        <span className="mt-6">YOUR EMAIL</span>
-        <input className="" type={"email"} placeholder="email..."/>
-        <span className="mt-4">YOUR MESSAGE</span>
-        <textarea className="md:h-[200px]" type={"text"} placeholder="message..."/>
-        <button className="border border-black hover:bg-black hover:text-white duration-200 mx-auto px-2 mt-2 focus:bg-black focus:text-white">Send Email</button>
+      <div className="w-screen h-[300px] flex align-center justify-center">
+        <div id="contact" className="m-auto rounded-br-2xl bg-blue-200 border border-black flex-col flex justify-center md:h-[100px] md:w-[400px]">
+          <h2 className="mx-auto text-2xl font-bold">Get In Touch!</h2>
+          <span className="mt-4 mx-auto">Email: lautaroblasco98@gmail.com</span>
+        </div>
       </div>
-    </div>
   )
 }
 
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProjectCard = (repoData) => {
   return (
-    <div className="relative flex flex-col items-center mt-4 border border-black rounded-md p-4 bg-blue-950 text-white h-[280px] w-[260px] hover:scale-110 ease-in duration-500">
+    <div className="shadow-2xl hover:border-white hover:border relative flex flex-col items-center mt-4 border border-black rounded-md p-4 bg-blue-950 text-white h-[280px] w-[260px] hover:scale-110 ease-in duration-500">
       <h1 className="font-bold text-xl">{repoData.title}</h1>
       <div className="w-full h-1 bg-white mt-2"></div>
       <p className="mt-4">{repoData.text}</p>
