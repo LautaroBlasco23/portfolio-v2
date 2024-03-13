6f91a3e62cb5ac91e301722e52e802a03194735d
lautaro <lautaroblasco98@gmail.com>
2024-03-13 10:17:41 -0300

    basic esthetic changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,9 +4,9 @@ const Contact = () => {
 
   return (
       <div className="w-screen h-[300px] flex align-center justify-center">
-        <div id="contact" className="m-auto rounded-br-2xl bg-neutral-400 border border-black flex-col flex justify-center md:h-[100px] md:w-[400px]">
-          <h2 className="mx-auto text-2xl font-bold">Get In Touch!</h2>
-          <span className="mt-4 mx-auto">Email: lautaroblasco98@gmail.com</span>
+        <div id="contact" className="m-auto rounded-br-2xl bg-neutral-700 border border-black flex-col flex justify-center md:h-[100px] md:w-[400px]">
+          <h2 className="mx-auto text-2xl font-bold text-neutral-300">Get In Touch!</h2>
+          <span className="mt-4 mx-auto text-neutral-100">Email: lautaroblasco98@gmail.com</span>
         </div>
       </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
       <span className="cursor-pointer hover:text-white duration-300" ><a href="#projects-container">Projects</a></span>
       <span className="cursor-pointer hover:text-white duration-300" ><a href="#contact">Contact</a></span>
       <div className="flex justify-end">
-        <span className="text-white mr-8 hover:cursor-pointer hover:text-blue-400 duration-300"><a href="https://drive.google.com/file/d/10JA8m5yGGzQPWOFieXqtgtX_p-MWfc4d/view?usp=sharing" target={"_blank"}>CV</a></span>
-        <span className="text-white hover:cursor-pointer hover:text-blue-400 duration-300"><a href="https://www.linkedin.com/in/lautaro-blasco-739644206/" target={"_blank"}>Linkedin</a></span>
+        <span className="text-white mr-8 hover:cursor-pointer hover:text-blue-400 duration-300"><a href="https://drive.google.com/file/d/10JA8m5yGGzQPWOFieXqtgtX_p-MWfc4d/view?usp=sharing" target={"_blank"} rel="noreferrer">CV</a></span>
+        <span className="text-white hover:cursor-pointer hover:text-blue-400 duration-300"><a href="https://www.linkedin.com/in/lautaro-blasco-739644206/" target={"_blank"} rel="noreferrer">Linkedin</a></span>
       </div>
     </div>
   )
